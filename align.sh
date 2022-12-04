775c3180e5782b6d35500d407a9d56c41e60b439
Shan Huang <shan.huang@desy.de>
2022-12-05 01:14:01 +0200

    Cumulative Updates in 2022
    Files renamed; bash script updated; detector updated

--- align.sh
+++ align.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
+export PATH_TO_TB=/srv01/tau/huang/beamtest/dataset/telescope
 
-run=$1
+if [[ $# == 2]]; then
+    run=$1
+    energy=$2
+    trackmodel="straightline"
+elif [[ $# == 3 ]]; then
+    run=$1
+    energy=$2
+    trackmodel=$3
+else
+    echo "Usage: $0 {Run number (4 digits)} {Energy (GeV)} [gbl/straightline (default)]"
+    exit 0
+fi
 
-corry -c 01-prealignment.conf -o output_directory="output_${run}" -o EventLoaderEUDAQ2.file_name="${PATH_TO_TB}/telescope/run000${run}_ni.raw";
-corry -c 02-alignment0.conf   -o output_directory="output_${run}" -o EventLoaderEUDAQ2.file_name="${PATH_TO_TB}/telescope/run000${run}_ni.raw";
-corry -c 03-alignment1.conf   -o output_directory="output_${run}" -o EventLoaderEUDAQ2.file_name="${PATH_TO_TB}/telescope/run000${run}_ni.raw";
-corry -c 04-alignment2.conf   -o output_directory="output_${run}" -o EventLoaderEUDAQ2.file_name="${PATH_TO_TB}/telescope/run000${run}_ni.raw";
+# TODO: padding run number to 6 digits
+corry -c 01-pre_align.conf -o output_directory="output_${run}" -o EventLoaderEUDAQ2.file_name="${PATH_TO_TB}/run00${run}_ni.raw";
+corry -c 02-align_pos.conf -o output_directory="output_${run}" -o EventLoaderEUDAQ2.file_name="${PATH_TO_TB}/run00${run}_ni.raw" -o Tracking4D.track_model=$trackmodel -o Tracking4D.momentum="${energy}GeV";
+corry -c 03-align_ang.conf -o output_directory="output_${run}" -o EventLoaderEUDAQ2.file_name="${PATH_TO_TB}/run00${run}_ni.raw" -o Tracking4D.track_model=$trackmodel -o Tracking4D.momentum="${energy}GeV";
+corry -c 04-align_posang.conf -o output_directory="output_${run}" -o EventLoaderEUDAQ2.file_name="${PATH_TO_TB}/run00${run}_ni.raw" -o Tracking4D.track_model=$trackmodel -o Tracking4D.momentum="${energy}GeV";
 
 cp -r geometries "output_${run}/"
-
--- run.sh
+++ run.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
+export PATH_TO_TB=/srv01/tau/huang/beamtest/dataset/telescope
 
-run=$1
+if [[ $# == 2]]; then
+    run=$1
+    energy=$2
+    trackmodel="straightline"
+elif [[ $# == 3 ]]; then
+    run=$1
+    energy=$2
+    trackmodel=$3
+else
+    echo "Usage: $0 {Run number (4 digits)} {Energy (GeV)} [gbl/straightline (default)]"
+    exit 0
+fi
 
-corry -c 05-run.conf  -o output_directory="output_${run}" -o EventLoaderEUDAQ2.file_name="${PATH_TO_TB}/telescope/run000${run}_ni.raw";
-
+# TODO: padding run number to 6 digits
+corry -c 05-run.conf  -o output_directory="output_${run}" -o EventLoaderEUDAQ2.file_name="${PATH_TO_TB}/run00${run}_ni.raw" -o Tracking4D.track_model=$trackmodel -o Tracking4D.momentum="${energy}GeV";
