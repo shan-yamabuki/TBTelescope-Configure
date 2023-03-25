0cd836f4be13860fa282cec2338facce1b53c9b7
Shan Huang <shan.huang@desy.de>
2023-03-25 16:37:50 +0200

    Minor correction of bash script

--- align.sh
+++ align.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-export PATH_TO_TB=/srv01/tau/huang/beamtest/dataset/telescope
 
 if [[ $# == 2 ]]; then
     run=$1
@@ -16,9 +15,9 @@ fi
 
 # TODO: padding run number to 6 digits
 corry -c 01-align-pre.conf -o output_directory="output_${run}" -o EventLoaderEUDAQ2.file_name="${PATH_TO_TB}/run00${run}_ni.raw";
-corry -c 02-align_chis200.conf -o output_directory="output_${run}" -o EventLoaderEUDAQ2.file_name="${PATH_TO_TB}/run00${run}_ni.raw" -o Tracking4D.track_model=$trackmodel -o Tracking4D.momentum="${energy}GeV";
-corry -c 03-align_chis100.conf -o output_directory="output_${run}" -o EventLoaderEUDAQ2.file_name="${PATH_TO_TB}/run00${run}_ni.raw" -o Tracking4D.track_model=$trackmodel -o Tracking4D.momentum="${energy}GeV";
+#corry -c 02-align_chis200.conf -o output_directory="output_${run}" -o EventLoaderEUDAQ2.file_name="${PATH_TO_TB}/run00${run}_ni.raw" -o Tracking4D.track_model=$trackmodel -o Tracking4D.momentum="${energy}GeV";
+#corry -c 03-align_chis100.conf -o output_directory="output_${run}" -o EventLoaderEUDAQ2.file_name="${PATH_TO_TB}/run00${run}_ni.raw" -o Tracking4D.track_model=$trackmodel -o Tracking4D.momentum="${energy}GeV";
 corry -c 04-align_chis50.conf -o output_directory="output_${run}" -o EventLoaderEUDAQ2.file_name="${PATH_TO_TB}/run00${run}_ni.raw" -o Tracking4D.track_model=$trackmodel -o Tracking4D.momentum="${energy}GeV";
 corry -c 05-align_millepede.conf -o output_directory="output_${run}" -o EventLoaderEUDAQ2.file_name="${PATH_TO_TB}/run00${run}_ni.raw" -o Tracking4D.track_model=$trackmodel -o Tracking4D.momentum="${energy}GeV";
 
-cp -r geometries "output_${run}/"
+cp -r geo "output_${run}/"
